a7e542ef614145ce71e522e57628a1d68dc62848
See Xiong <seexiong@Sees-MacBook-Air.local>
2019-04-17 16:26:09 -0500

    stuck on sorting wonAmount array

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,85 +1,101 @@
 var dice1 = 0;
 var dice2 = 0;
 var betAmount = document.getElementById('amount');
+var playButton = document.getElementById('playBtn');
+var betButton = document.getElementById('betBtn');
 //results
 var money = 0;
 var clickPlayBtn = 0;
 //each time you roll dice, the count get recorded here
 var rollsBeforeBroke = [];
-var highestAmountWon = [];
+var wonAmounts = [];
+//store the largest wonAmount element inside of highestAmountWon
+var highestAmountWon = 0;
 var rollCountAtHighest = [];
 
 
-//FUNCTIONS ===================================================
+//FUNCTIONS that are used multiple times =====================================
 
-//update money on DOM
 function updateMoney() {
     document.getElementById('yourMoney-text').innerHTML = money;
 };
-
-function win() {
-    money += 4;
-    console.log("you win $4, now you have $" + money)
-}
-
-function loss() {
-    money--;
-    console.log("you loss $1, now you have $" + money)
-}
+function printResults() {
+    document.getElementById('endMessage').innerHTML = "You ran out of money.";
+    document.getElementById('startingBet-text').innerHTML = betAmount.value;
+    document.getElementById('totalRolls-text').innerHTML = clickPlayBtn;
+    document.getElementById('highestAmount-text').innerHTML = highestAmountWon;
+    document.getElementById('rollCountAtHighest-text').innerHTML = rollCountAtHighest;
+};
+function resetResults() {
+    clickPlayBtn = 0;
+    rollsBeforeBroke = [];
+    wonAmounts = [];
+    highestAmountWon = 0;
+    rollCountAtHighest = [];
+    printResults();
+};
 
 //roll dice
     //dice roll random number, then display it on DOM
-    //if sum is 7 then you win, if not you lose
+    //if sum is 7 then you win, or else you lose
     //if money is 0, then print out results
 function rollDice() {
-    
     dice1 = Math.ceil(Math.random()*6);
     dice2 = Math.ceil(Math.random()*6);
     document.getElementById('dice1-text').innerHTML = dice1;
     document.getElementById('dice2-text').innerHTML = dice2;
 
     if (dice1 + dice2 == 7) {
-        win();
+        //YOU WIN $4
+        money+=4;
+        console.log("you win $4, now you have $" + money)
         updateMoney();
     } else {
-        loss();
+        //YOU LOSE $1
+        money--;
+        console.log("you loss $1, now you have $" + money)
         updateMoney();
     };
 
     if (money == 0 ) {
         console.log("--You're OUT OF money!--")
-        document.getElementById('endMessage').innerHTML = "You ran out of money.";
-        document.getElementById('startingBet-text').innerHTML = betAmount.value;
-        document.getElementById('totalRolls-text').innerHTML = clickPlayBtn;
-        document.getElementById('highestAmount-text').innerHTML = highestAmountWon;
-        document.getElementById('rollCountAtHighest-text').innerHTML = rollCountAtHighest;
+        //sort wonAmounts array and store last element (which is the largest
+        //amount) inside of the var highestAmountWon
+        wonAmounts.sort();
+        console.log("SORTED: "+ wonAmounts.sort())
+        highestAmountWon = wonAmounts[wonAmounts.length-1];
+        console.log("Largest Amount Won: $" + highestAmountWon)
+        printResults();
     }
+};
 
-}
 
 //============================================================================
 //GAME START HERE ============================================================
 
-var playButton = document.getElementById('playBtn');
-var betButton = document.getElementById('betBtn');
-
-//Use betAmount for money, BET button uses that money to bet
+//When BET button is click - run the function
+    //use the vaule from input field as money
+    //update on DOM and reset the results
 betButton.addEventListener('click', function() {
     money = betAmount.value;
     updateMoney();
-
+    resetResults();
 })
 
-//When PLAY button is clicked, 
-    //if money is greater than 0 then roll the dices and play
-    //if money is less than zero then they need to place a bet
+//When PLAY button is clicked - run the function
+    //if money is greater than 0    
+        //keep track of the round being play in an array called wonAmounts
+        //then roll the dices and play
+    //of else if money is less than zero then they need to place a bet
 
 playButton.addEventListener('click', function() {
     if (money > 0) {
         clickPlayBtn++;
+        wonAmounts.push(money);
+        console.log("Current Money: " + wonAmounts);
         rollDice();
         console.log("play: " + clickPlayBtn)
-        // rollsBeforeBroke.push(clickPlayBtn);
+        console.log("===============================")
     }
     else {
         alert("You need to place a bet.")
